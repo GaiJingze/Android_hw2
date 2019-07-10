c3c543558b44063048d6102e1bad402ff1b8a41e
1725802722 <1725802722@qq.com>
2019-07-10 16:02:22 +0800

    homework 3-1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,14 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.graphics.Color;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         ObjectAnimator animator1 = ObjectAnimator.ofFloat(view,"scaleX",1,3,1);
         ObjectAnimator animator2 = ObjectAnimator.ofFloat(view,"scaleY",3,1,3);
         ObjectAnimator animator3 = ObjectAnimator.ofFloat(view,"alpha",1,0,1);
+        animator1.setRepeatCount(ValueAnimator.INFINITE);
+        animator2.setRepeatCount(ValueAnimator.INFINITE);
+        animator3.setRepeatCount(ValueAnimator.INFINITE);
         AnimatorSet set = new AnimatorSet();
         set.playTogether(animator1, animator2, animator3);
         set.setDuration(1000);
